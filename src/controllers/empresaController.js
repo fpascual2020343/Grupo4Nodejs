5f7d3fee634b3de66ebed93215ad45edf9a7aec3
Franshesco Pascual <fpascual-2020343@kinal.edu.gt>
2022-04-01 13:12:49 -0600

    Cambiando las validaciones por un fallo

--- src/controllers/empresaController.js
+++ src/controllers/empresaController.js
@@ -4,78 +4,66 @@ const jwt = require('../services/jwt')
 const res = require('express/lib/response');
 const Empresa = require('../models/empresa.model')
 
-function agregarEmpresa(req, res){
-    const parametros = req.body; 
+function agregarEmpresa(req, res) {
+    const parametros = req.body;
     const modeloEmpresa = new Empresa();
 
-if(req.user.rol == 'SuperAdmin'){
     modeloEmpresa.nombre = parametros.nombre;
-    modeloEmpresa.direccion = parametros.direccion; 
-    modeloEmpresa.descripcion = parametros.descripcion; 
-    modeloEmpresa.rol = 'Empresa'; 
+    modeloEmpresa.direccion = parametros.direccion;
+    modeloEmpresa.descripcion = parametros.descripcion;
+    modeloEmpresa.rol = 'Empresa';
 
-    modeloEmpresa.save((err, empresaGuardada)=>{
-        if(err) return res.status(500).send({mensaje: 'Hubo un error en la peticion'})
-        if(!empresaGuardada) return res.status(500).send({mensaje: 'Hubo un error al agregar la empresa'})
+    modeloEmpresa.save((err, empresaGuardada) => {
+        if (err) return res.status(500).send({ mensaje: 'Hubo un error en la peticion' })
+        if (!empresaGuardada) return res.status(500).send({ mensaje: 'Hubo un error al agregar la empresa' })
 
-        return res.status(200).send({empresa: empresaGuardada})
+        return res.status(200).send({ empresa: empresaGuardada })
     })
 
-}else{
-    return res.status(500).send({mensaje: 'Solo los administradores pueden agregar empresas'})
-}
 
-   
 
 }
-function editarEmpresa(req, res){
-    const parametros = req.body; 
+
+function editarEmpresa(req, res) {
+    const parametros = req.body;
     const idEmpresa = req.params.idEmpresa;
-    if(req.user.rol = 'SuperAdmin'){
+    if (req.user.rol = 'SuperAdmin') {
 
-        Empresa.findByIdAndUpdate(idEmpresa, parametros, {new: true}, (err, empresaActualizada)=>{
-            if(err) return res.status(500).send({mensaje: 'Hubo en la peticion'});
-            if(!empresaActualizada) res.status(500).send({mensaje: 'Hubo un error al actualizar la empresa'})
+        Empresa.findByIdAndUpdate(idEmpresa, parametros, { new: true }, (err, empresaActualizada) => {
+            if (err) return res.status(500).send({ mensaje: 'Hubo en la peticion' });
+            if (!empresaActualizada) res.status(500).send({ mensaje: 'Hubo un error al actualizar la empresa' })
 
-            return res.status(200).send({empresa: empresaActualizada})
+            return res.status(200).send({ empresa: empresaActualizada })
         })
 
-    }else{
-        return res.status(500).send({mensaje: 'Solo los admnistradores pueden editar las empresas'})
+    } else {
+        return res.status(500).send({ mensaje: 'Solo los admnistradores pueden editar las empresas' })
     }
 }
 
-function eliminarEmpresa(req, res){
-    const idEmpresa = req.params.idEmpresa; 
+function eliminarEmpresa(req, res) {
+    const idEmpresa = req.params.idEmpresa;
 
-    if(req.user.rol == 'SuperAdmin'){
-        Empresa.findByIdAndDelete({_id: idEmpresa}, (err, empresaEliminada)=>{
-            if(err) return res.status(500).send({mensaje: 'Hubo un error en la peticion'});
-            if(!empresaEliminada) return res.status(500).send({mensaje: 'Hubo un error al eliminar la empresa'}); 
-    
-            return res.status(200).send({empresa: empresaEliminada})
+    if (req.user.rol == 'SuperAdmin') {
+        Empresa.findByIdAndDelete({ _id: idEmpresa }, (err, empresaEliminada) => {
+            if (err) return res.status(500).send({ mensaje: 'Hubo un error en la peticion' });
+            if (!empresaEliminada) return res.status(500).send({ mensaje: 'Hubo un error al eliminar la empresa' });
+
+            return res.status(200).send({ empresa: empresaEliminada })
         })
-    }else{
-        return res.status(500).send({mensaje: 'Solo los administradores pueden eliminar las empresas'})
+    } else {
+        return res.status(500).send({ mensaje: 'Solo los administradores pueden eliminar las empresas' })
     }
 
 }
-function obtenerEmpresas(req, res){
+function obtenerEmpresas(req, res) {
 
-    if(req.user.rol == 'SuperAdmin'){
+    Empresa.find({}, (err, empresaEncontradas) => {
+        if (err) return res.status(500).send({ mensaje: 'Hubo un error en la peticion' })
+        if (!empresaEncontradas) return res.status(500).send({ mensaje: 'Hubo un error al obtener las empresas' })
 
-        Empresa.find({}, (err, empresaEncontradas)=>{
-            if(err) return res.status(500).send({mensaje: 'Hubo un error en la peticion'})
-            if(!empresaEncontradas) return res.status(500).send({mensaje: 'Hubo un error al obtener las empresas'})
-
-            return res.status(200).send({empresa: empresaEncontradas})
-        })
-
-    }else{
-
-        return res.status(500).send({mensaje: 'Solo los administradores pueden ver las categorias'})
-
-    }
+        return res.status(200).send({ empresa: empresaEncontradas })
+    })
 }
 module.exports = {
     agregarEmpresa,
