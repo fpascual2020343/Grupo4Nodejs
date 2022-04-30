f82ebefb77d2ba722e7fe7165f7f2694da880daa
hugo <daniel.patza@outlook.es>
2022-04-29 23:28:41 -0600

    generar venta

--- src/routes/sucursales.routes.js
+++ src/routes/sucursales.routes.js
@@ -8,9 +8,9 @@ api.post('/agregarSucursal', md_autenticacion.Auth, sucursalesControlador.agrega
 api.put('/editarSucursal/:idSucursal', md_autenticacion.Auth, sucursalesControlador.editarSucursal);//editar sucursal
 api.get('/obtenerSucursales', md_autenticacion.Auth,sucursalesControlador.obtenerSucursales);//obtener sucursales
 api.delete('/eliminarSucursales/:idSucursal', md_autenticacion.Auth, sucursalesControlador.eliminarSucursal); //eliminar sucursales
-api.get('/obtenerProductos/:idSucursal', md_autenticacion.Auth, sucursalesControlador.obtenerSucursales);//obtener productos
+api.get('/obtenerProductos/:idSucursal', md_autenticacion.Auth, sucursalesControlador.obtenerProductos);//obtener productos
 api.get('/obtenerSucursal/:idSucursal', md_autenticacion.Auth,sucursalesControlador.ObtenerSucursalId);//obtener sucursales
-
-
+api.put('/generarVenta/:idProducto/:idSucursal', md_autenticacion.Auth, sucursalesControlador.generarVenta)
+api.get('/obtenerProdid/:idProducto/:idSucursal', md_autenticacion.Auth, sucursalesControlador.obtenerProductoId)
 
 module.exports = api;
